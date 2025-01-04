691a2adb2f0401f26a9d52eea8630e43ea84afa6
Mohammad <mrhz1376@gmail.com>
2025-01-04 17:11:06 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,36 @@
+import { ReactTyped } from "react-typed";
+function Hero() {
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data.
+        </h1>
+        <div className="flex justify-center items-center gap-2">
+          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+            Fast, flexible financing for
+          </p>
+          <ReactTyped
+            className="md:text-5xl sm:text-4xl text-xl font-bold"
+            strings={["BTB", "SASS", "BTC"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
+        </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your Data Analytics to increase revenue for BTB, BTC, SASS
+          platforms.
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black">
+          Get Started
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
